903a6e8f6961146d3c97541cc6fe058592e4c597
Antoine Poussard <antoinepoussard@MacBook-Pro-de-Antoine.local>
2018-03-05 12:28:09 +0100

    Fraction class + Unit test project

---
+++ LangageObjCours1/LangageObjCours1/Fraction.cs
@@ -0,0 +1,93 @@
+﻿using System;
+namespace LangageObjCours1
+{
+
+    public class Fraction
+    {
+        public int Numerator { get; set; }
+        public int Denominator{ get; set; }
+
+        public Fraction(int x, int y)
+        {
+            this.Numerator = x;
+            this.Denominator = y;
+        }
+
+        public Fraction(string s)
+        {
+            string[] pieces = s.Split('/');
+            this.Numerator = int.Parse(pieces[0]);
+            this.Denominator = int.Parse(pieces[1]);
+        }
+
+        // Return PGCD
+        private static int PGCD(int a, int b)
+        {
+            int temp = a % b;
+            if (temp == 0)
+                return b;
+            return PGCD(b, temp);
+        }
+
+        // Simplify the fraction
+        public void Simplifier() {
+            // Simplify the sign.
+            if (this.Denominator < 0)
+            {
+                this.Numerator = -this.Numerator;
+                this.Denominator = -this.Denominator;
+            }
+
+            // Factor out the greatest common divisor of the
+            // numerator and the denominator.
+            int gcd_ab = PGCD(this.Numerator, this.Denominator);
+            this.Numerator = this.Numerator / gcd_ab;
+            this.Denominator = this.Denominator / gcd_ab;
+        }
+
+
+        // Return a * b
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            // Swap numerators and denominators to simplify.
+            Fraction result1 = new Fraction(a.Numerator, b.Denominator);
+            Fraction result2 = new Fraction(b.Numerator, a.Denominator);
+
+            return new Fraction(
+                result1.Numerator * result2.Numerator,
+                result1.Denominator * result2.Denominator);
+        }
+
+        // Return a + b
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            // Get the denominators' greatest common divisor.
+            int gcd_ab = PGCD(a.Denominator, b.Denominator);
+
+            int numer =
+                a.Numerator * (b.Denominator / gcd_ab) +
+                b.Numerator * (a.Denominator / gcd_ab);
+            int denom =
+                a.Denominator * (b.Denominator / gcd_ab);
+            return new Fraction(numer, denom);
+        }
+
+        // Return -a
+        public static Fraction operator -(Fraction a)
+        {
+            return new Fraction(-a.Numerator, a.Denominator);
+        }
+
+        // Return a - b
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            return a + -b;
+        }
+
+        // Return a / b.
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            return a * new Fraction(b.Denominator, b.Numerator);
+        }
+    }
+}
---
+++ LangageObjCours1/TestLOCours1/UnitTest1.cs
@@ -0,0 +1,96 @@
+using LangageObjCours1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestLOCours1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestMethod]
+        public void Simplifier()
+        {
+            Fraction f = new Fraction(4, 4);
+            f.Simplifier();
+            Assert.AreEqual(1, f.Denominator);
+            Assert.AreEqual(1, f.Numerator);
+        }
+
+        [TestMethod]
+        public void FractionString() {
+            Fraction f = new Fraction("1/2");
+            Assert.AreEqual(1, f.Numerator);
+            Assert.AreEqual(2, f.Denominator);
+        }
+
+        [TestMethod]
+        public void MultiplyFraction() {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(1, 4);
+            Fraction test = f1 * f2;
+            Assert.AreEqual(8, test.Denominator);
+            Assert.AreEqual(1, test.Numerator);
+        }
+
+        [TestMethod]
+        public void MultiplyFractionSimplified()
+        {
+            Fraction f1 = new Fraction(1, 4);
+            Fraction f2 = new Fraction(4, 1);
+            Fraction test = f1 * f2;
+            test.Simplifier();
+            Assert.AreEqual(1, test.Denominator);
+            Assert.AreEqual(1, test.Numerator);
+        }
+
+        [TestMethod]
+        public void AddFraction()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(1, 4);
+            Fraction test = f1 + f2;
+            Assert.AreEqual(4, test.Denominator);
+            Assert.AreEqual(3, test.Numerator);
+        }
+
+        [TestMethod]
+        public void SubstractFraction()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(1, 4);
+            Fraction test = f1 - f2;
+            Assert.AreEqual(4, test.Denominator);
+            Assert.AreEqual(1, test.Numerator);
+        }
+
+        [TestMethod]
+        public void InverseFraction() {
+            Fraction f = new Fraction(1, 2);
+            f = -f;
+            Assert.AreEqual(2, f.Denominator);
+            Assert.AreEqual(-1, f.Numerator);
+        }
+
+        [TestMethod]
+        public void DivideFraction() {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(1, 4);
+            Fraction test = f1 / f2;
+            Assert.AreEqual(2, test.Denominator);
+            Assert.AreEqual(4, test.Numerator);
+        }
+
+        [TestMethod]
+        public void DivideFractionSimplified()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(1, 4);
+            Fraction test = f1 / f2;
+            test.Simplifier();
+            Assert.AreEqual(1, test.Denominator);
+            Assert.AreEqual(2, test.Numerator);
+        }
+
+
+    }
+}
